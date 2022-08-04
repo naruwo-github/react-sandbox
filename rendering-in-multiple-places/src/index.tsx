e5ce74382764878b47a8fa37a187f8334aae13d7
channaru <narutyan0711@gmail.com>
2022-08-05 07:21:42 +0900

    rendering by foreach

--- rendering-in-multiple-places/src/index.tsx
+++ rendering-in-multiple-places/src/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { App } from './App'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const domContents = document.querySelectorAll('#selected')
+
+domContents.forEach(element => {
+  const root = ReactDOM.createRoot(element)
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+})
