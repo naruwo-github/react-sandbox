301d9546533d31d5e78a630737e7e97d4cc43814
channaru <narutyan0711@gmail.com>
2022-08-05 07:58:48 +0900

    update app explanation on readme

--- rendering-in-multiple-places/src/App.tsx
+++ rendering-in-multiple-places/src/App.tsx
@@ -2,17 +2,13 @@ import React from 'react';
 
 export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React.js 公式サイト
-        </a>
-      </header>
-    </div>
-  );
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      React.js Official Site
+    </a>
+  )
 }
