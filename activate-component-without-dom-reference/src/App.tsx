f595b673d1db6f3bd275962e51d1931a2da60942
channaru <narutyan0711@gmail.com>
2022-08-07 11:35:58 +0900

    remove jsx

--- activate-component-without-dom-reference/src/App.tsx
+++ activate-component-without-dom-reference/src/App.tsx
@@ -1,14 +1,5 @@
 export const App = () => {
   console.log("In the App()")
 
-  return (
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      React.js Official Site
-    </a>
-  )
+  return null
 }
