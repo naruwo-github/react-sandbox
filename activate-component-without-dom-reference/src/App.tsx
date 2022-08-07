e50b44c2776301aafd71543131b959b291d17020
channaru <narutyan0711@gmail.com>
2022-08-07 11:40:36 +0900

    update comments

--- activate-component-without-dom-reference/src/App.tsx
+++ activate-component-without-dom-reference/src/App.tsx
@@ -1,5 +1,6 @@
 export const App = () => {
-  console.log("In the App()")
+  console.log("Since the DOM element we're rendering to isn't visible anywhere, ")
+  console.log("returning a JSX element here won't render it.")
 
   return null
 }
