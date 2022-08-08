5ff2a0e70fd4beded9185c9fa4ce910a621f3701
ChanNaru <narutyan0711@gmail.com>
2022-08-09 08:00:04 +0900

    extract directories

--- trial-and-error-for-directory-structure/src/index.tsx
+++ trial-and-error-for-directory-structure/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import App from './app/App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
+import App from './app/components/App';
+import reportWebVitals from './assets/tools/reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
