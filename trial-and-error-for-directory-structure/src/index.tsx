8d0bea4f18fc80b5c79294c9c29bbacb638bc866
ChanNaru <narutyan0711@gmail.com>
2022-08-09 08:08:00 +0900

    update structure

--- trial-and-error-for-directory-structure/src/index.tsx
+++ trial-and-error-for-directory-structure/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './assets/styles/index.css';
 import App from './app/components/App';
 import reportWebVitals from './assets/tools/reportWebVitals';
 
