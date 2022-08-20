740f6005d5259c962b76307f59c68516e421bac0
ChanNaru <narutyan0711@gmail.com>
2022-08-20 11:06:06 +0900

    add swr and fetched users

--- useSWR-samples/src/App.tsx
+++ useSWR-samples/src/App.tsx
@@ -1,5 +1,11 @@
+import useSWR from 'swr'
+
+const fetcher = (...args: [string]) => fetch(...args).then(res => res.json())
+
 export const App = () => {
-  return (
-    <></>
-  )
+  const {data, error} = useSWR('http://localhost:3001/users', fetcher)
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+  return <div>hello {data}!</div>
 }
