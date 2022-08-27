0a8ff4def807e363ea950618b8e41637132dfc2f
ChanNaru <narutyan0711@gmail.com>
2022-08-27 17:23:04 +0900

    copy sample code

---
+++ clean-architecture/src/driver/articleDriver.ts
@@ -0,0 +1,8 @@
+import ArticleDriver, { ArticlesJson } from "../interface/driver/articleDriver"
+
+export default class ArticleDriverImpl implements ArticleDriver {
+    async findAll(): Promise<ArticlesJson> {
+        const res = await fetch("http://localhost:3000/articles")
+        return await res.json()
+    }
+}
