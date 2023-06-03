2da637bdf934e4c54bb5f308316403a13a0e417d
ChanNaru <narutyan0711@gmail.com>
2023-06-03 22:07:43 +0900

    update testing

--- app-with-e2e-and-ui-testing/tests/pc-nextPage/nextPage.spec.ts
+++ app-with-e2e-and-ui-testing/tests/pc-nextPage/nextPage.spec.ts
@@ -1,17 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
 	await page.goto('http://localhost:5173/index2.html');
+});
+
+test('has title', async ({ page }) => {
 	await expect(page).toHaveTitle('Vite + React + TS');
 });
 
 test('count button', async ({ page }) => {
-	await page.goto('http://localhost:5173/index2.html');
 	await page.getByRole('button', { name: 'count is 0' }).click();
 	expect(await page.textContent('body')).toContain('count is 1');
 });
 
 test('snapshot', async ({ page }) => {
-	await page.goto('http://localhost:5173/index2.html');
-	expect(await page.screenshot()).toMatchSnapshot('./snapshots/nextPage.png');
+	expect(await page.screenshot()).toMatchSnapshot({
+		name: 'nextPage.png'
+	});
 });
--- app-with-e2e-and-ui-testing/tests/pc-top/top.spec.ts
+++ app-with-e2e-and-ui-testing/tests/pc-top/top.spec.ts
@@ -1,17 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
 	await page.goto('http://localhost:5173/');
+});
+
+test('has title', async ({ page }) => {
 	await expect(page).toHaveTitle('Vite + React + TS');
 });
 
 test('move to nextPage', async ({ page }) => {
-	await page.goto('http://localhost:5173/');
 	await page.getByRole('link', { name: 'Go to starter sample.' }).click();
 	expect(page.url()).toBe('http://localhost:5173/index2.html');
 });
 
 test('snapshot', async ({ page }) => {
-	await page.goto('http://localhost:5173/');
-	expect(await page.screenshot()).toMatchSnapshot('./snapshots/top.png');
+	expect(await page.screenshot()).toMatchSnapshot({
+		name: 'page.png'
+	});
 });
