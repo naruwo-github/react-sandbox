1979b53f041d81406b2ad56a2e4eceaa549e29ca
ChanNaru <narutyan0711@gmail.com>
2023-06-03 22:28:32 +0900

    install eslint prettier settings

--- app-with-e2e-and-ui-testing/tests/pc-nextPage/nextPage.spec.ts
+++ app-with-e2e-and-ui-testing/tests/pc-nextPage/nextPage.spec.ts
@@ -4,17 +4,17 @@ test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:5173/index2.html')
 })
 
-test('has title', async ({ page }) => {
+test('The page should have the correct title', async ({ page }) => {
   await expect(page).toHaveTitle('Vite + React + TS')
 })
 
-test('count button', async ({ page }) => {
+test('Count button should increment the count', async ({ page }) => {
   await page.getByRole('button', { name: 'count is 0' }).click()
   expect(await page.textContent('body')).toContain('count is 1')
 })
 
-test('snapshot', async ({ page }) => {
+test('The page should match the stored snapshot', async ({ page }) => {
   expect(await page.screenshot()).toMatchSnapshot({
-    name: 'nextPage.png',
+    name: 'nextPage.png'
   })
 })
--- app-with-e2e-and-ui-testing/tests/pc-top/top.spec.ts
+++ app-with-e2e-and-ui-testing/tests/pc-top/top.spec.ts
@@ -4,16 +4,18 @@ test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:5173/')
 })
 
-test('has title', async ({ page }) => {
+test('The page should have the correct title', async ({ page }) => {
   await expect(page).toHaveTitle('Vite + React + TS')
 })
 
-test('move to nextPage', async ({ page }) => {
+test('The user should be able to navigate to the next page', async ({
+  page
+}) => {
   await page.getByRole('link', { name: 'Go to starter sample.' }).click()
   expect(page.url()).toBe('http://localhost:5173/index2.html')
 })
 
-test('snapshot', async ({ page }) => {
+test('The page should match the stored snapshot', async ({ page }) => {
   expect(await page.screenshot()).toMatchSnapshot({
     name: 'page.png'
   })
