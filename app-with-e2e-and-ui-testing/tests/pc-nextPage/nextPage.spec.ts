ecf689a7a9c3fa19a3ca852be413b6932cf10d9f
ChanNaru <narutyan0711@gmail.com>
2023-06-03 22:22:33 +0900

    install eslint prettier settings

--- app-with-e2e-and-ui-testing/tests/pc-nextPage/nextPage.spec.ts
+++ app-with-e2e-and-ui-testing/tests/pc-nextPage/nextPage.spec.ts
@@ -1,20 +1,20 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
 test.beforeEach(async ({ page }) => {
-	await page.goto('http://localhost:5173/index2.html');
-});
+  await page.goto('http://localhost:5173/index2.html')
+})
 
 test('has title', async ({ page }) => {
-	await expect(page).toHaveTitle('Vite + React + TS');
-});
+  await expect(page).toHaveTitle('Vite + React + TS')
+})
 
 test('count button', async ({ page }) => {
-	await page.getByRole('button', { name: 'count is 0' }).click();
-	expect(await page.textContent('body')).toContain('count is 1');
-});
+  await page.getByRole('button', { name: 'count is 0' }).click()
+  expect(await page.textContent('body')).toContain('count is 1')
+})
 
 test('snapshot', async ({ page }) => {
-	expect(await page.screenshot()).toMatchSnapshot({
-		name: 'nextPage.png'
-	});
-});
+  expect(await page.screenshot()).toMatchSnapshot({
+    name: 'nextPage.png',
+  })
+})
--- app-with-e2e-and-ui-testing/tests/pc-top/top.spec.ts
+++ app-with-e2e-and-ui-testing/tests/pc-top/top.spec.ts
@@ -1,20 +1,20 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
 test.beforeEach(async ({ page }) => {
-	await page.goto('http://localhost:5173/');
-});
+  await page.goto('http://localhost:5173/')
+})
 
 test('has title', async ({ page }) => {
-	await expect(page).toHaveTitle('Vite + React + TS');
-});
+  await expect(page).toHaveTitle('Vite + React + TS')
+})
 
 test('move to nextPage', async ({ page }) => {
-	await page.getByRole('link', { name: 'Go to starter sample.' }).click();
-	expect(page.url()).toBe('http://localhost:5173/index2.html');
-});
+  await page.getByRole('link', { name: 'Go to starter sample.' }).click()
+  expect(page.url()).toBe('http://localhost:5173/index2.html')
+})
 
 test('snapshot', async ({ page }) => {
-	expect(await page.screenshot()).toMatchSnapshot({
-		name: 'page.png'
-	});
-});
+  expect(await page.screenshot()).toMatchSnapshot({
+    name: 'page.png'
+  })
+})
