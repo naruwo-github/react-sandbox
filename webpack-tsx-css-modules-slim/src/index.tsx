4606676117bd1af34ba788ed540dbcfbdfcc7aed
ChanNaru <narutyan0711@gmail.com>
2023-06-10 08:29:24 +0900

    define html to check app on browser

--- webpack-tsx-css-modules-slim/src/index.tsx
+++ webpack-tsx-css-modules-slim/src/index.tsx
@@ -5,10 +5,12 @@ import styles from './index.module.scss'
 const App = () => (
   <div className={styles.app}>
     <h1>Hello, world!</h1>
-    <button className={styles.button}>Click me!</button>
+    <button className={styles.button} onClick={() => alert('Button clicked!')}>
+      Click me!
+    </button>
     <div className={styles.inputContainer}>
       <label htmlFor="name" className={styles.label}>Your name:</label>
-      <input id="name" type="text" className={styles.input} />
+      <input id="name" type="text" placeholder="Type here..." className={styles.input} />
     </div>
   </div>
 )
