1b2a83e81f94fd3ea39b15a94af702082fb59cdc
ChanNaru <narutyan0711@gmail.com>
2023-06-10 08:06:53 +0900

    define html to check app on browser

--- webpack-tsx-css-modules-slim/src/index.tsx
+++ webpack-tsx-css-modules-slim/src/index.tsx
@@ -4,7 +4,12 @@ import styles from './index.module.scss'
 
 const App = () => (
   <div className={styles.app}>
-    Hello, world!
+    <h1>Hello, world!</h1>
+    <button className={styles.button}>Click me!</button>
+    <div className={styles.inputContainer}>
+      <label htmlFor="name" className={styles.label}>Your name:</label>
+      <input id="name" type="text" className={styles.input} />
+    </div>
   </div>
 )
 
--- webpack-tsx-css-modules-slim/webpack.config.js
+++ webpack-tsx-css-modules-slim/webpack.config.js
@@ -1,55 +1,56 @@
 const path = require('path')
 
 module.exports = {
-  mode:  'development', // 'production' | 'development' | 'none'
-  entry: './src/index.tsx',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.jsx'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'swc-loader',
-            options: {
-              jsc: {
-                parser: {
-                  syntax: 'typescript',
-                  tsx: true,
-                },
-                target: 'es2015',
-              },
-              env: {
-                targets: {
-                  browsers: ['last 2 versions', 'safari >= 7', 'IE 11'],
-                },
-              },
-            },
-          },
-        ],
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                auto: /\.module\.\w+$/i,
-              }
-            },
-          },
-          'sass-loader',
-        ],
-      },
-    ],
-  },
+	mode: 'development', // 'production' | 'development' | 'none'
+	entry: './src/index.tsx',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js', '.jsx']
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'swc-loader',
+						options: {
+							jsc: {
+								parser: {
+									syntax: 'typescript',
+									tsx: true
+								},
+								target: 'es2015'
+							},
+							env: {
+								targets: {
+									browsers: ['last 2 versions', 'safari >= 7', 'IE 11']
+								}
+							}
+						}
+					}
+				]
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					'style-loader',
+					{
+						loader: 'css-loader',
+						options: {
+							modules: {
+								auto: /\.module\.\w+$/i,
+								localIdentName: '[local]__[hash:base64:5]'
+							}
+						}
+					},
+					'sass-loader'
+				]
+			}
+		]
+	}
 };
